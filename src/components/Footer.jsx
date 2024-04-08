f51f7ca7d34a18900e8fed52f959d199711bd893
Code-with-Nani <usmanhanan02@gmail.com>
2024-04-08 05:31:26 +0500

    fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { items , list } from "../assets/data/data";
 
 const Footer = () => {
   return (
-    <div className="h-[85vh] w-[100vw] bg-[#131313] lg:px-20 px-4 ">
+    <div className="min-h-[85vh] w-[100vw] bg-[#131313] lg:px-20 px-4  ">
       <div className="upper  h-[65vh] w-full flex flex-col lg:flex-row lg:py-20 py-16 list-none gap-10 text-white">
         <div className="left h-full lg:w-1/2 ">
           <h2 className="regular text-[13px] leading-[16px] mb-4">
@@ -35,7 +35,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="lower  flex flex-col justify-center items-start lg:items-end lg:mt-16 mt-32 gap-2 pb-5">
+      <div className="lower  flex flex-col justify-center items-start lg:items-end lg:mt-16 mt-52 gap-2 pb-5">
         <h2 className="text-[#6d6d6e] regular text-[12px] leading-[16px]">
           {" "}
           © Copyright AB Volvo 2024
